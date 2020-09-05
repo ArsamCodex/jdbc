7a4e04de3a0c6e0c85c3c2dbcaa4e2e3e35b7266
ARII <62879887+ArsamCodex@users.noreply.github.com>
2020-09-05 22:07:45 +0200

    II

--- src/main/java/Model1/javaaproach/be/Bussines/Main.java
+++ src/main/java/Model1/javaaproach/be/Bussines/Main.java
@@ -1,22 +1,32 @@
 package Model1.javaaproach.be.Bussines;
 
 
+import Model1.javaaproach.be.Model.Course;
+import Model1.javaaproach.be.Model.Exam;
+import Model1.javaaproach.be.Model.Gender;
+import Model1.javaaproach.be.Model.Person;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+
 public class Main {
     public static  void main (String [] args){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("dataSource");
         EntityManager em = emf.createEntityManager();
-
-
+        Person person = new Person().setFamilyName("MAS").setFirstName("RA").setGender(Gender.MALE);
         EntityTransaction et = em.getTransaction();
         et.begin();
+        em.persist(person);
 
         et.commit();
         em.close();
         emf.close();
+        System.out.println(person.toString());
+
+
+
     }
 }
