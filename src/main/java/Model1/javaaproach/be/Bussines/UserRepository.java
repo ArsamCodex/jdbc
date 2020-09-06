a6310b93d84b60ef6cfbf3678c4d7ff4a56afe62
ARII <62879887+ArsamCodex@users.noreply.github.com>
2020-09-06 15:36:21 +0200

    IV

--- src/main/java/Model1/javaaproach/be/Bussines/UserRepository.java
+++ src/main/java/Model1/javaaproach/be/Bussines/UserRepository.java
@@ -4,8 +4,11 @@ Public class UserRepository{
 
 
 // WOrking whith smartfoon typo
-   Public static void outputExam(User user){
-   
+   Public static void outputExam(User 
+
+
+     EntityManagerFactory emf = Persistence.createEntityManagerFactory("dataSource");
+     EntityManager em = emf.createEntityManager();
 
 
  }
